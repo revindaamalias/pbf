ff704145e259a76b5695c39df5db93b229ae04f5
revindaamalias <revindaamalia@gmail.com>
2021-03-18 09:50:12 +0700

    desain

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
-// import './index.css'
+import { Button } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import dark_green from './assets/dark_green_.jpg';
+import light_blue from './assets/light_blue_.jpg';
+import light_grey from './assets/light_grey_.jpg';
+import light_purple from './assets/light_purple_.jpg';
+import skin from './assets/skin_.jpg';
+
+import cheese from './assets/cheese.jpg';
+import deep_coffee from './assets/deep_coffee_.jpg';
+import mulberry from './assets/mulberry_.jpg';
+import sky from './assets/sky_.jpg';
+import sunflower from './assets/Sunflower_.jpg';
+
+import Background from './assets/Home.jpg';
 
 import {
     BrowserRouter as Router,
@@ -32,26 +44,19 @@ function MarketPlace() {
                         </a>
                     </li>
                     <li class="nav-item">
-                        <Link class="nav-link" href="/about">About</Link>
-                    </li>
-                    <li class="nav-item">
-                        <Link class="nav-link" href="/product">Product</Link>
-                    </li>
-                    <li class="nav-item">
-                        <Link class="nav-link" href="/contact">Contact</Link>
+                        <Link class="nav-link" to="/product">Product</Link>
                     </li>
                     </ul>
-                    <AuthButton/>
                 </div>
-
-
+                </div>
+                <AuthButton/>
+            </nav>
+            
                 <Switch>
                     <Route exact path="/home" component={Home} />
                     <Route path="/login" component={Login} />
                     <PrivateRoute path="/product" component={Product} />
                 </Switch>
-            </div>
-            </nav>
         </Router>
     )
 }
@@ -70,8 +75,9 @@ const fakeAuth = {
 
 const AuthButton = withRouter(({ history }) =>
     fakeAuth.isAuthenticated ? (
-        <p id="logout">Welcome!
+        <p id="logout">
             <button
+                className='btn btn-warning'
                 onClick={() => {
                     fakeAuth.signout(() => history.push("/home"));
                 }}>
@@ -118,23 +124,23 @@ function Home() {
                     </ol>
                     <div className="carousel-inner" role="listbox">
                         <div className="carousel-item active">
-                            <img className="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide"/>
+                            <img className="d-block img-fluid" src={Background} alt="First slide"/>
                         </div>
                         <div className="carousel-item">
                             <img className="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide"/>
                         </div>
-                        <div className="carousel-item">
-                            <img className="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide"/>
+                        <div class="carousel-item">
+                            <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide"/>
                         </div>
                     </div>
-                    <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="sr-only">Previous</span>
-                    </a>
-                    <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="sr-only">Next</span>
-                    </a>
+                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
                 </div>
             </div>
             </div>
@@ -153,23 +159,195 @@ function Product() {
                 <div className="list-group">
                     <ul>
                         <li className="list-group-item">
-                            <Link to={`${url}/NSG`}>Natural Short Glossy</Link>
+                            <Link to={`/product/NSG`}>Natural Short Glossy</Link>
                         </li>
                         <li className="list-group-item">
-                            <Link to={`${url}/OSM`}>Oval Short Matte</Link>
+                            <Link to={`/product/OSM`}>Oval Short Matte</Link>
                         </li>
                     </ul>
-
-                    <Switch>
-                        <Route exact path={path}>
-
-                        </Route>
-                        <Route path={`${path}/:dataId`}>
-                            <DataBody />
-                        </Route>
-                    </Switch>
                 </div>
             </div>
+            <div className='col-lg-9'>
+                <Switch>
+                    <Route exact path={`/product/NSG`}>
+                        <br />
+                        <div className='container'>
+                            <div className="row">
+                                <div className="col-lg-4 col-md-6 mb-4">
+                                    <div className="card h-100">
+                                        <img src={dark_green} alt="gambar"/>
+                                        <div className="card-body">
+                                            <h4 className="card-title">
+                                                <a>Dark Green</a>
+                                            </h4>
+                                            <h5>Rp 30.000</h5>
+                                            <p className="card-text">Natural Short Glossy - Dark Green</p>
+                                        </div>
+                                        <div className="card-footer">
+                                            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-4 col-md-6 mb-4">
+                                    <div className="card h-100">
+                                        <img src={light_blue} alt="gambar"/>
+                                        <div className="card-body">
+                                            <h4 className="card-title">
+                                                <a>Light Blue</a>
+                                            </h4>
+                                            <h5>Rp 30.000</h5>
+                                            <p className="card-text">Natural Short Glossy - Light Blue</p>
+                                        </div>
+                                        <div className="card-footer">
+                                            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-4 col-md-6 mb-4">
+                                    <div className="card h-100">
+                                        <img src={light_grey} alt="gambar"/>
+                                        <div className="card-body">
+                                            <h4 className="card-title">
+                                                <a>Light Grey</a>
+                                            </h4>
+                                            <h5>Rp 30.000</h5>
+                                            <p className="card-text">Natural Short Glossy - Light Grey</p>
+                                        </div>
+                                        <div className="card-footer">
+                                            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-4 col-md-6 mb-4">
+                                    <div className="card h-100">
+                                        <img src={light_purple} alt="gambar"/>
+                                        <div className="card-body">
+                                            <h4 className="card-title">
+                                                <a>Light Purple</a>
+                                            </h4>
+                                            <h5>Rp 30.000</h5>
+                                            <p className="card-text">Natural Short Glossy - Purple</p>
+                                        </div>
+                                        <div className="card-footer">
+                                            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-4 col-md-6 mb-4">
+                                    <div className="card h-100">
+                                        <img src={skin} alt="gambar"/>
+                                        <div className="card-body">
+                                            <h4 className="card-title">
+                                                <a>Skin</a>
+                                            </h4>
+                                            <h5>Rp 30.000</h5>
+                                            <p className="card-text">Natural Short Glossy - Skin</p>
+                                        </div>
+                                        <div className="card-footer">
+                                            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Route>
+                    <Route exact path={`/product/OSM`}>
+                        <br />
+                    <div className='container'>
+                            <div className="row">
+                                <div className="col-lg-4 col-md-6 mb-4">
+                                    <div className="card h-100">
+                                        <img src={cheese} alt="gambar"/>
+                                        <div className="card-body">
+                                            <h4 className="card-title">
+                                                <a>Cheese</a>
+                                            </h4>
+                                            <h5>Rp 30.000</h5>
+                                            <p className="card-text">Oval Short Matte - Chese</p>
+                                        </div>
+                                        <div className="card-footer">
+                                            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-4 col-md-6 mb-4">
+                                    <div className="card h-100">
+                                        <img src={mulberry} alt="gambar"/>
+                                        <div className="card-body">
+                                            <h4 className="card-title">
+                                                <a>Mulberry</a>
+                                            </h4>
+                                            <h5>Rp 30.000</h5>
+                                            <p className="card-text">Oval Short Matte - Mulberry</p>
+                                        </div>
+                                        <div className="card-footer">
+                                            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-4 col-md-6 mb-4">
+                                    <div className="card h-100">
+                                        <img src={sunflower} alt="gambar"/>
+                                        <div className="card-body">
+                                            <h4 className="card-title">
+                                                <a>Sunflower</a>
+                                            </h4>
+                                            <h5>Rp 30.000</h5>
+                                            <p className="card-text">Oval Short Matte - Sunflower</p>
+                                        </div>
+                                        <div className="card-footer">
+                                            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-4 col-md-6 mb-4">
+                                    <div className="card h-100">
+                                        <img src={sky} alt="gambar"/>
+                                        <div className="card-body">
+                                            <h4 className="card-title">
+                                                <a>Sky</a>
+                                            </h4>
+                                            <h5>Rp 30.000</h5>
+                                            <p className="card-text">Oval Short Matte - Sky</p>
+                                        </div>
+                                        <div className="card-footer">
+                                            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-4 col-md-6 mb-4">
+                                    <div className="card h-100">
+                                        <img src={deep_coffee} alt="gambar"/>
+                                        <div className="card-body">
+                                            <h4 className="card-title">
+                                                <a>Deep Coffee</a>
+                                            </h4>
+                                            <h5>Rp 30.000</h5>
+                                            <p className="card-text">Oval Short Matte - Deep Coffee</p>
+                                        </div>
+                                        <div className="card-footer">
+                                            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Route>
+                    <Route exact path={path}></Route>
+                    <Route exact path={path}></Route>
+                    <Route path={`${path}/:dataId`}>
+                        <DataBody />
+                     </Route>
+                </Switch>
+            </div>   
         </div>
     )
 }
@@ -245,6 +423,9 @@ class Login extends Component {
                   <div className="container">
                       <p className="m-0 text-center text-while">You Must Login First</p>
                   </div>
+                  <button
+                    className='btn btn-primary'
+                    onClick={this.login}> Login </button>
               </footer>
         </div>
         );
